91dc522dea52c976c7af1f3c55ee9c8c031efbc8
trongnhanuit <trongnhan.uit@gmail.com>
2014-09-21 17:30:56 +0700

    Update08-2109
    Cuoi buoi

--- src/com/android/raspicontrol/StreamingFrame.java
+++ src/com/android/raspicontrol/StreamingFrame.java
@@ -1,14 +1,20 @@
 package com.android.raspicontrol;
 
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 
 
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class StreamingFrame extends Fragment {
 	
 	WebView webview;
@@ -20,6 +26,7 @@ public class StreamingFrame extends Fragment {
 				false);
 		return rootView;
 	}
+	@SuppressLint("NewApi")
 	@Override
 	public void setUserVisibleHint(boolean isVisibleToUser) 
 	{
@@ -32,13 +39,14 @@ public class StreamingFrame extends Fragment {
 	            webview.getSettings().setDomStorageEnabled(true);
 	            //webview.getSettings().setBuiltInZoomControls(true);
 	            webview.getSettings().setJavaScriptEnabled(true);
-	            webview.loadUrl("http://192.168.1.101:8080/");
-	            /*String strHtml = "<html><head>"
+	            
+	            //webview.loadUrl("http://192.168.1.101:8080/");
+	            String strHtml = "<html><head>"
 					          + "</head>"
 					          + "<body>"
 					          + "<img src='http://192.168.1.101:8080/?action=stream'/>"            
-					          + "</body></html>";*/
-				//webview.loadData(strHtml, "text/html", null);
+					          + "</body></html>";
+				webview.loadData(strHtml, "text/html", null);
         }
 	}
 }
