635f76d706cab5b55a5e129833ffb8bcb9bc64ce
Dao Duy Vuong <itharry@live.com>
2014-09-21 14:41:18 -0700

    Update05

--- src/com/android/raspicontrol/StreamingFrame.java
+++ src/com/android/raspicontrol/StreamingFrame.java
@@ -29,8 +29,8 @@ public class StreamingFrame extends Fragment {
         {
 	    	 Log.d("MyFragment", "stream");
 	            webview=(WebView)rootView.findViewById(R.id.streamview);
-	            webview.getSettings().setDomStorageEnabled(true);
-	            webview.getSettings().setBuiltInZoomControls(true);
+	            //webview.getSettings().setDomStorageEnabled(true);
+	            //webview.getSettings().setBuiltInZoomControls(true);
 	            //webview.loadUrl("http://192.168.1.101:8080/");
 	            String strHtml = "<html><head>"
 					          + "</head>"
